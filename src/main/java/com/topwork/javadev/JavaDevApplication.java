868396ac1e943accfadfa39d28ed9cc08f55b472
licyclone <licyclone@163.com>
2018-09-08 18:01:39 +0800

    springboot

---
+++ src/main/java/com/topwork/javadev/JavaDevApplication.java
@@ -0,0 +1,13 @@
+package com.topwork.javadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  JavaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevApplication.class, args);
+	}
+
+}
